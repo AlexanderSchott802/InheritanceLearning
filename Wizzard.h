d5bc3c81c05c36212f229815354794bea56d9966
Alexander Schott <alexanderschott@alexanders-air.wireless.weber.edu>
2023-01-27 08:50:23 -0700

    Added header files

---
+++ Wizzard.h
@@ -0,0 +1,8 @@
+//
+// Created by Alexander Schott on 1/27/23.
+//
+
+#ifndef INHERITANCELEARNING_WIZZARD_H
+#define INHERITANCELEARNING_WIZZARD_H
+
+#endif //INHERITANCELEARNING_WIZZARD_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <ctime>
+
+#include "Wizzard.h"
+#include "Character.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
