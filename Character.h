d8b77c0f6107c75f40fa9fd6aab0ad11ea22def0
Alexander Schott <alexanderschott@alexanders-air.wireless.weber.edu>
2023-01-27 09:05:44 -0700

    Added all the functions from the fist video, implementing the character class

--- Character.h
+++ Character.h
@@ -8,9 +8,41 @@
 #include "iostream"
 class Character{
 public:
+    Character() : name("Unnamed"), hp(0){}
+    Character(std::string uName, int uHp) : name(uName), hp(uHp){}
 
+    virtual void startBattle(){
+        attack = (rand() % 10) + 2;
+        defence = (rand() % 10) + 2;
+    }
+
+    virtual void takeDamage(int damage){
+        hp -= damage;
+    }
+
+    void battle(Character *enemy){
+        if(attack > enemy->defence){
+            int damage = attack - enemy->defence;
+            enemy->takeDamage(damage);
+            std::cout << name << " Hits for " << damage << " damage!";
+        }else{
+            std::cout << name << " attempts to hit, but is blocked!";
+        }
+    }
+
+    int getHp(){
+        return hp;
+    }
+
+    friend std::ostream &operator << (std::ostream output, const Character &c){
+        output << c.name << " " << c.hp << std::endl;
+        return output;
+    }
 protected:
-
+    std::string name;
+    int defence;
+    int attack;
+    int hp;
 };
 
 
